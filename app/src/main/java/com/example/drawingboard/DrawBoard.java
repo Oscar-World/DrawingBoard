ae0ecbededfa701429fdbcb5504b85a603322815
Oscar <rkdgjdl@naver.com>
2022-10-26 19:21:52 +0900

    modify main function

--- app/src/main/java/com/example/drawingboard/DrawBoard.java
+++ app/src/main/java/com/example/drawingboard/DrawBoard.java
@@ -18,25 +18,36 @@ public class DrawBoard extends View {
     Paint p3 = new Paint();
     Paint p4 = new Paint();
     Paint p5 = new Paint();
+    Paint p6 = new Paint();
+    Paint p7 = new Paint();
+    Paint p8 = new Paint();
+    Paint p9 = new Paint();
+    Paint p10 = new Paint();
 
     int data_X[] = new int[30000];
     int data_Y[] = new int[30000];
     int data_Color[] = new int[30000];
+    int data_Radius[] = new int[30000];
 
     static int radius = 15;
 
-    int dataNum = 0;
+    static int dataNum = 0;
     int mx, my;
     static int whatColor = 0;
 
     public DrawBoard(Context context, AttributeSet attr) {
         super(context);
 
-        p1.setColor(Color.BLACK);
-        p2.setColor(Color.RED);
-        p3.setColor(Color.BLUE);
-        p4.setColor(Color.GREEN);
-        p5.setColor(Color.YELLOW);
+        p1.setColor(getResources().getColor(R.color.black));
+        p2.setColor(getResources().getColor(R.color.white));
+        p3.setColor(getResources().getColor(R.color.red));
+        p4.setColor(getResources().getColor(R.color.orange));
+        p5.setColor(getResources().getColor(R.color.yellow));
+        p6.setColor(getResources().getColor(R.color.green));
+        p7.setColor(getResources().getColor(R.color.blue));
+        p8.setColor(getResources().getColor(R.color.purple));
+        p9.setColor(getResources().getColor(R.color.brown));
+        p10.setColor(getResources().getColor(R.color.apricot));
 
         data_X[0] = 0;
         data_Y[0] = 0;
@@ -51,23 +62,43 @@ public class DrawBoard extends View {
         for (int i=1; i <= dataNum; i++) {
 
             if (data_Color[i] == 0) {
-                canvas.drawCircle(data_X[i], data_Y[i], radius, p1);
+                canvas.drawCircle(data_X[i], data_Y[i], data_Radius[i], p1);
             }
 
             if (data_Color[i] == 1) {
-                canvas.drawCircle(data_X[i], data_Y[i], radius, p2);
+                canvas.drawCircle(data_X[i], data_Y[i], data_Radius[i], p2);
             }
 
             if (data_Color[i] == 2) {
-                canvas.drawCircle(data_X[i], data_Y[i], radius, p3);
+                canvas.drawCircle(data_X[i], data_Y[i], data_Radius[i], p3);
             }
 
             if (data_Color[i] == 3) {
-                canvas.drawCircle(data_X[i], data_Y[i], radius, p4);
+                canvas.drawCircle(data_X[i], data_Y[i], data_Radius[i], p4);
             }
 
             if (data_Color[i] == 4) {
-                canvas.drawCircle(data_X[i], data_Y[i], radius, p5);
+                canvas.drawCircle(data_X[i], data_Y[i], data_Radius[i], p5);
+            }
+
+            if (data_Color[i] == 5) {
+                canvas.drawCircle(data_X[i], data_Y[i], data_Radius[i], p6);
+            }
+
+            if (data_Color[i] == 6) {
+                canvas.drawCircle(data_X[i], data_Y[i], data_Radius[i], p7);
+            }
+
+            if (data_Color[i] == 7) {
+                canvas.drawCircle(data_X[i], data_Y[i], data_Radius[i], p8);
+            }
+
+            if (data_Color[i] == 8) {
+                canvas.drawCircle(data_X[i], data_Y[i], data_Radius[i], p9);
+            }
+
+            if (data_Color[i] == 9) {
+                canvas.drawCircle(data_X[i], data_Y[i], data_Radius[i], p10);
             }
 
         }
@@ -81,6 +112,7 @@ public class DrawBoard extends View {
         data_X[dataNum] = mx;
         data_Y[dataNum] = my;
         data_Color[dataNum] = whatColor;
+        data_Radius[dataNum] = radius;
 
     }
 
@@ -91,13 +123,11 @@ public class DrawBoard extends View {
 
         dataNum +=1;
         saveData();
-        Log.d(TAG, "onTouchEvent: " + dataNum);
-        Log.d(TAG, "mx: " + mx);
-        Log.d(TAG, "my: " + my);
+        Log.d(TAG, "onTouchEvent: " + dataNum + " / mx : " + mx + " / my : " + my + " / radius : " + radius + " / color : " + whatColor);
+
         return true;
 
     }
-
 
 
 }
--- app/src/main/java/com/example/drawingboard/MainActivity.java
+++ app/src/main/java/com/example/drawingboard/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.drawingboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.res.ColorStateList;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,61 +20,125 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button resetBtn = findViewById(R.id.reset_Btn);
-        Button increaseBtn = findViewById(R.id.increase_Btn);
-        Button decreaseBtn = findViewById(R.id.decrease_Btn);
+        LinearLayout set_Linear = findViewById(R.id.set_Linear);
+//        LinearLayout color_Linear = findViewById(R.id.color_Linear);
+
+        ImageButton saveBtn = findViewById(R.id.save_Btn);
+        ImageButton resetBtn = findViewById(R.id.reset_Btn);
+        ImageButton increaseBtn = findViewById(R.id.increase_Btn);
+        ImageButton decreaseBtn = findViewById(R.id.decrease_Btn);
+        Button colorBtn = findViewById(R.id.color_Btn);
         Button blackBtn = findViewById(R.id.black_Btn);
+        Button whiteBtn = findViewById(R.id.white_Btn);
         Button redBtn = findViewById(R.id.red_Btn);
-        Button blueBtn = findViewById(R.id.blue_Btn);
-        Button greenBtn = findViewById(R.id.green_Btn);
+        Button orangeBtn = findViewById(R.id.orange_Btn);
         Button yellowBtn = findViewById(R.id.yellow_Btn);
+        Button greenBtn = findViewById(R.id.green_Btn);
+        Button blueBtn = findViewById(R.id.blue_Btn);
+        Button purpleBtn = findViewById(R.id.purple_Btn);
+        Button brownBtn = findViewById(R.id.brown_Btn);
+        Button apricotBtn = findViewById(R.id.apricot_Btn);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        View drawerView = findViewById(R.id.color_drawer);
+
+        SeekBar seekBar = findViewById(R.id.radius_Seekbar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                // 시크바 조작 중
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // 시크바 조작 시작 시
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // 시크바 조작 끝났을 시
+            }
+        });
 
         View.OnClickListener Click = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (view.getId() == R.id.color_Btn) {
+//                    set_Linear.setVisibility(View.GONE);
+//                    color_Linear.setVisibility(View.VISIBLE);
 
-                switch (view.getId()) {
+                    drawerLayout.openDrawer(drawerView);
 
-                    case R.id.reset_Btn:
+                } else {
+                    if (view.getId() == R.id.save_Btn) {
 
-                        break;
-                    case R.id.increase_Btn:
-                        DrawBoard.radius += 2;
-                        break;
-                    case R.id.decrease_Btn:
-                        DrawBoard.radius -= 2;
-                        break;
-                    case R.id.black_Btn:
+
+                    } else if (view.getId() == R.id.reset_Btn) {
+                        DrawBoard.dataNum = 0;
+                    } else if (view.getId() == R.id.increase_Btn) {
+                        if (DrawBoard.radius < 29) {
+                            DrawBoard.radius += 2;
+                        }
+                    } else if (view.getId() == R.id.decrease_Btn) {
+                        if (DrawBoard.radius > 3) {
+                            DrawBoard.radius -= 2;
+                        }
+                    } else if (view.getId() == R.id.black_Btn) {
                         DrawBoard.whatColor = 0;
-                        break;
-                    case R.id.red_Btn:
+                        colorBtn.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.black)));
+                    } else if (view.getId() == R.id.white_Btn) {
                         DrawBoard.whatColor = 1;
-                        break;
-                    case R.id.blue_Btn:
+                        colorBtn.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.white)));
+                    } else if (view.getId() == R.id.red_Btn) {
                         DrawBoard.whatColor = 2;
-                        break;
-                    case R.id.green_Btn:
+                        colorBtn.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.red)));
+                    } else if (view.getId() == R.id.orange_Btn) {
                         DrawBoard.whatColor = 3;
-                        break;
-                    case R.id.yellow_Btn:
+                        colorBtn.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.orange)));
+                    } else if (view.getId() == R.id.yellow_Btn) {
                         DrawBoard.whatColor = 4;
-                        break;
+                        colorBtn.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.yellow)));
+                    } else if (view.getId() == R.id.green_Btn) {
+                        DrawBoard.whatColor = 5;
+                        colorBtn.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.green)));
+                    } else if (view.getId() == R.id.blue_Btn) {
+                        DrawBoard.whatColor = 6;
+                        colorBtn.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.blue)));
+                    } else if (view.getId() == R.id.purple_Btn) {
+                        DrawBoard.whatColor = 7;
+                        colorBtn.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.purple)));
+                    } else if (view.getId() == R.id.brown_Btn) {
+                        DrawBoard.whatColor = 8;
+                        colorBtn.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.brown)));
+                    } else if (view.getId() == R.id.apricot_Btn) {
+                        DrawBoard.whatColor = 9;
+                        colorBtn.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.apricot)));
+                    }
+
+                    drawerLayout.closeDrawer(drawerView);
+//                set_Linear.setVisibility(View.VISIBLE);
+//                color_Linear.setVisibility(View.GONE);
 
                 }
 
             }
         };
 
-
+        saveBtn.setOnClickListener(Click);
         resetBtn.setOnClickListener(Click);
         increaseBtn.setOnClickListener(Click);
         decreaseBtn.setOnClickListener(Click);
+        colorBtn.setOnClickListener(Click);
         blackBtn.setOnClickListener(Click);
+        whiteBtn.setOnClickListener(Click);
         redBtn.setOnClickListener(Click);
-        blueBtn.setOnClickListener(Click);
-        greenBtn.setOnClickListener(Click);
+        orangeBtn.setOnClickListener(Click);
         yellowBtn.setOnClickListener(Click);
-
+        greenBtn.setOnClickListener(Click);
+        blueBtn.setOnClickListener(Click);
+        purpleBtn.setOnClickListener(Click);
+        brownBtn.setOnClickListener(Click);
+        apricotBtn.setOnClickListener(Click);
 
     }
 }
